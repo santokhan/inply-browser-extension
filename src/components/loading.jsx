6f2930a85c49fb2fe265ece647cf24c4cf701cef
Santo Khan <santokhan1999@gmail.com>
2026-06-02 18:36:50 +0600

    Update ui

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-full text-sm text-gray-500">
+    <div className="flex items-center justify-center h-full text-sm text-gray-500 p-4">
       Loading...
     </div>
   );
